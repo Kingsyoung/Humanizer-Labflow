174e1eb2aa85eafdc08ba5f9f7c2b089305f129b
Kingsyoung <116896508+Kingsyoung@users.noreply.github.com>
2026-05-25 07:05:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-code = '''import os
+import os
 import re
 import json
 import random
@@ -498,5 +498,4 @@ if __name__ == "__main__":
     import uvicorn
     port = int(os.getenv("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
-'''
 
